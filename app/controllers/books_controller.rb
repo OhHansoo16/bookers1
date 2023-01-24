dd4b1a323b336b6a008ec964085c7c1835cdf50d
EC2 Default User <ec2-user@ip-172-31-34-42.ap-northeast-1.compute.internal>
2023-01-24 03:44:08 +0000

    [Finish] Create機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,42 @@
 class BooksController < ApplicationController
-  def index
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Submitted successfully!"
+      #redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
+  def index
+    @books = Book.all
+  end
+
+=begin
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+=end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
